2673d0a05e194b4bd04e37a5627a97fa3085389b
hfmckenna <hfmckenna@outlook.com>
2021-10-02 17:13:53 +0100

    4.18 Complete

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const config = require("./utils/config");
 const express = require("express");
 require("express-async-errors");
 const app = express();
+const loginRouter = require("./controllers/login");
 const blogsRouter = require("./controllers/blogs");
 const usersRouter = require("./controllers/users");
 const cors = require("cors");
@@ -25,6 +26,7 @@ app.use(express.static("build"));
 app.use(express.json());
 app.use(middleware.requestLogger);
 
+app.use('/api/login', loginRouter)
 app.use("/api/blogs", blogsRouter);
 app.use("/api/users", usersRouter);
 
